95f3c19868477e36ae44c1362e5b4664ceb9eac8
Brian Wong <brianw7218@gmail.com>
2023-02-24 22:26:59 -0800

    Add basic testing routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Shop from "./pages/Shop";
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
