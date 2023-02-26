c6997fdf6cb599658370430420ddab4a638bf406
Brian Wong <brianw7218@gmail.com>
2023-02-25 22:27:17 -0800

    Add nav bar component that will always be displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Shop from "./pages/Shop";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shop />} />
