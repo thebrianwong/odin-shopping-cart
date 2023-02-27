236f3d8cf2ce76a3fc86766d9a9f204288b62a21
Brian Wong <brianw7218@gmail.com>
2023-02-26 20:44:26 -0800

    Display a message when there are no applicable champions to display based on the active tags

--- src/components/ShopContents.js
+++ src/components/ShopContents.js
@@ -1,10 +1,16 @@
 const ShopContents = ({ shopItems }) => {
   return (
-    <ul>
-      {Object.keys(shopItems).map((item) => {
-        return <li key={shopItems[item].key}>{shopItems[item].name}</li>;
-      })}
-    </ul>
+    <>
+      {Object.keys(shopItems).length > 0 ? (
+        <ul>
+          {Object.keys(shopItems).map((item) => {
+            return <li key={shopItems[item].key}>{shopItems[item].name}</li>;
+          })}
+        </ul>
+      ) : (
+        <div>placeholder no items text</div>
+      )}
+    </>
   );
 };
 
