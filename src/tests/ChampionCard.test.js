9ebccd9286188d2ed6f441b1b3ab1cad24e72a80
Brian Wong <brianw7218@gmail.com>
2023-02-26 22:17:27 -0800

    Add test for champion card component

---
+++ src/tests/ChampionCard.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import ChampionCard from "../components/ChampionCard";
+
+let aatroxData;
+
+beforeAll(async () => {
+  const rawData = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/13.4.1/data/en_US/champion.json"
+  );
+  const parsedJSON = await rawData.json();
+  const championData = parsedJSON.data;
+  aatroxData = championData.Aatrox;
+});
+
+test("Champion card gets rendered properly", () => {
+  render(<ChampionCard championData={aatroxData} />);
+  const aatroxCard = screen.getByRole("button");
+  const aatroxImage = screen.getByAltText("Square portrait of Aatrox");
+  const aatroxName = screen.getByText("Aatrox");
+  expect(aatroxCard).toBeInTheDocument();
+  expect(aatroxImage).toBeInTheDocument();
+  expect(aatroxName).toBeInTheDocument();
+});
