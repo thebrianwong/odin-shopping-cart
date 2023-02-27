043188410059862bfd41c2f99f34eefb5c83cb3e
Brian Wong <brianw7218@gmail.com>
2023-02-26 18:56:19 -0800

    Fix tests with mock functions

--- src/tests/ShopFilter.test.js
+++ src/tests/ShopFilter.test.js
@@ -4,8 +4,11 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import ShopFilter from "../components/ShopFilter";
 
+const addFilter = jest.fn();
+const removeFilter = jest.fn();
+
 test("All labels are rendered", () => {
-  render(<ShopFilter />);
+  render(<ShopFilter addFilter={addFilter} removeFilter={removeFilter} />);
   const tags = ["Assassin", "Fighter", "Mage", "Marksman", "Support", "Tank"];
   const labels = [];
   for (let i = 0; i < tags.length; i++) {
@@ -16,13 +19,13 @@ test("All labels are rendered", () => {
 });
 
 test("All checkboxes are rendered", () => {
-  render(<ShopFilter />);
+  render(<ShopFilter addFilter={addFilter} removeFilter={removeFilter} />);
   const checkboxes = screen.getAllByRole("checkbox");
   expect(checkboxes.length).toBe(6);
 });
 
 test("Checkboxes can be checked", () => {
-  render(<ShopFilter />);
+  render(<ShopFilter addFilter={addFilter} removeFilter={removeFilter} />);
   const checkbox = screen.getByRole("checkbox", { name: "Assassin" });
   expect(checkbox.checked).toBeFalsy();
   userEvent.click(checkbox);
