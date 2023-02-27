8b760b7e7095dd19e23ec30fa4503fd88f914d45
Brian Wong <brianw7218@gmail.com>
2023-02-26 21:22:02 -0800

    Refactor shop contents component tests

--- src/tests/ShopContents.test.js
+++ src/tests/ShopContents.test.js
@@ -4,24 +4,24 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import ShopContents from "../components/ShopContents";
 
-test("Renders list items from prop object", async () => {
+let data;
+
+beforeAll(async () => {
   const rawData = await fetch(
     "http://ddragon.leagueoflegends.com/cdn/13.4.1/data/en_US/champion.json"
   );
   const parsedJSON = await rawData.json();
-  const infoObject = parsedJSON.data;
-  render(<ShopContents shopItems={infoObject} />);
+  data = parsedJSON.data;
+});
+
+test("Renders list items from prop object", async () => {
+  render(<ShopContents shopItems={data} />);
   const listItems = screen.getAllByRole("listitem");
   expect(listItems.length).toBe(162);
 });
 
 test("Renders list items with text", async () => {
-  const rawData = await fetch(
-    "http://ddragon.leagueoflegends.com/cdn/13.4.1/data/en_US/champion.json"
-  );
-  const parsedJSON = await rawData.json();
-  const infoObject = parsedJSON.data;
-  render(<ShopContents shopItems={infoObject} />);
+  render(<ShopContents shopItems={data} />);
   const listItems = screen.getAllByRole("listitem");
   listItems.forEach((item) => {
     expect(item).toHaveTextContent(/^.+$/);
