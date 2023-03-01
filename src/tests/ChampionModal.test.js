56a0b6cecc9e3a53cc11ded275df72dcdfac3baa
Brian Wong <brianw7218@gmail.com>
2023-02-28 21:42:33 -0800

    Add test for champion modal component

---
+++ src/tests/ChampionModal.test.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import ChampionModal from "../components/ChampionModal";
+import { act } from "react-dom/test-utils";
+
+const hideModal = jest.fn();
+const addToCart = jest.fn();
+
+let aatroxData;
+
+beforeAll(async () => {
+  const rawData = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/13.4.1/data/en_US/champion.json"
+  );
+  const parsedJSON = await rawData.json();
+  const championData = parsedJSON.data;
+  aatroxData = championData.Aatrox;
+});
+
+test("Champion modal is rendered properly", () => {
+  render(
+    <ChampionModal
+      championData={aatroxData}
+      hideModal={hideModal}
+      addToCart={addToCart}
+    />
+  );
+  const aatroxImage = screen.getByAltText("Loading screen banner of Aatrox");
+  const aatroxName = screen.getByRole("heading", { name: "Aatrox" });
+  const aatroxTitle = screen.getByText("the Darkin Blade");
+  const aatroxBlurb = screen.getByText(/Shurima/);
+  const buttons = screen.getAllByRole("button");
+  const input = screen.getByDisplayValue("1");
+  expect(aatroxImage).toBeInTheDocument();
+  expect(aatroxImage.src).toBe(
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg"
+  );
+  expect(aatroxName).toBeInTheDocument();
+  expect(aatroxTitle).toBeInTheDocument();
+  expect(aatroxBlurb).toBeInTheDocument();
+  expect(buttons.length).toBe(4);
+  expect(input).toBeInTheDocument();
+});
+
+test("Changing input value via buttons works", () => {
+  render(
+    <ChampionModal
+      championData={aatroxData}
+      hideModal={hideModal}
+      addToCart={addToCart}
+    />
+  );
+  const buttons = screen.getAllByRole("button");
+  const input = screen.getByDisplayValue("1");
+  const decrementButton = buttons[0];
+  const incrementButton = buttons[1];
+  act(() => {
+    userEvent.click(incrementButton);
+  });
+  expect(input.value).toBe("11");
+  act(() => {
+    userEvent.click(decrementButton);
+  });
+  expect(input.value).toBe("1");
+});
+
+test("The input value can't be decreased past 1 with the buttons", () => {
+  render(
+    <ChampionModal
+      championData={aatroxData}
+      hideModal={hideModal}
+      addToCart={addToCart}
+    />
+  );
+  const buttons = screen.getAllByRole("button");
+  const input = screen.getByDisplayValue("1");
+  const decrementButton = buttons[0];
+  expect(input.value).toBe("1");
+  act(() => {
+    userEvent.click(decrementButton);
+  });
+  expect(input.value).toBe("1");
+});
+
+test("The input value can be changed via typing", () => {
+  render(
+    <ChampionModal
+      championData={aatroxData}
+      hideModal={hideModal}
+      addToCart={addToCart}
+    />
+  );
+  const input = screen.getByDisplayValue("1");
+  expect(input.value).toBe("1");
+  input.focus();
+  act(() => {
+    userEvent.keyboard("12");
+  });
+  expect(input.value).toBe("112");
+});
