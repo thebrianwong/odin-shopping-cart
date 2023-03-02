da030b86aecca491643f781cd22242f92cffcc32
Brian Wong <brianw7218@gmail.com>
2023-03-01 21:30:08 -0800

    Add a checkout page that displays shopping cart contents

---
+++ src/pages/Checkout.js
@@ -0,0 +1,22 @@
+const Checkout = ({ data, shoppingCartItems }) => {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <ul>
+        {Object.keys(shoppingCartItems).map((champion) => {
+          return (
+            <li key={data[champion].key}>
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/13.4.1/img/champion/${data[champion].id}.png`}
+                alt={`Square portrait of ${data[champion]}`}
+              />
+              {champion}: {shoppingCartItems[champion]}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Checkout;
