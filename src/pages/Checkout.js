9dd753236ba91d34ae1d30c8efa1bd3dbbf9b106
Brian Wong <brianw7218@gmail.com>
2023-03-02 22:21:15 -0800

    Add empty message and prevent ability to place an order when the shopping cart is empty

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,24 +1,44 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CheckoutItem from "../components/CheckoutItem";
 
 const Checkout = ({ data, shoppingCartItems, changeCartQuantity }) => {
+  const [canPlaceOrder, setCanPlaceOrder] = useState(
+    Object.keys(shoppingCartItems).length
+  );
+  useEffect(() => {
+    setCanPlaceOrder(Object.keys(shoppingCartItems).length);
+  }, [shoppingCartItems]);
   return (
     <div>
       <h1>Checkout</h1>
-      <ul>
-        {Object.keys(shoppingCartItems).map((champion) => {
-          return (
-            <CheckoutItem
-              key={data[champion].key}
-              data={data}
-              champion={champion}
-              quantity={shoppingCartItems[champion]}
-              changeCartQuantity={changeCartQuantity}
-            />
-          );
-        })}
-      </ul>
-      <Link to="/orderconfirmation">
+      {canPlaceOrder ? (
+        <ul>
+          {Object.keys(shoppingCartItems).map((champion) => {
+            return (
+              <CheckoutItem
+                key={data[champion].key}
+                data={data}
+                champion={champion}
+                quantity={shoppingCartItems[champion]}
+                changeCartQuantity={changeCartQuantity}
+              />
+            );
+          })}
+        </ul>
+      ) : (
+        <p>
+          You cart is empty. Go to the shop and add some champions to your cart!
+        </p>
+      )}
+      <Link
+        onClick={(e) => {
+          if (!canPlaceOrder) {
+            e.preventDefault();
+          }
+        }}
+        to="/orderconfirmation"
+      >
         <button>Place Order</button>
       </Link>
     </div>
