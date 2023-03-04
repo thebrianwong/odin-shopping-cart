fe6bbfde92ae23ab8905925410ac701dc9a1413d
Brian Wong <brianw7218@gmail.com>
2023-03-03 22:23:28 -0800

    Combine tests into one that also accounts for newly added content

--- src/tests/Homepage.test.js
+++ src/tests/Homepage.test.js
@@ -5,14 +5,20 @@ import userEvent from "@testing-library/user-event";
 import Homepage from "../pages/Homepage";
 import { BrowserRouter } from "react-router-dom";
 
-test("The page renders with the correct title", () => {
+test("The page renders with the expected elements", () => {
   render(<Homepage />, { wrapper: BrowserRouter });
-  const title = screen.getByRole("heading", { name: "Welcome!" }).textContent;
-  expect(title).toMatch("Welcome!");
-});
-
-test("The page renders with a button", () => {
-  render(<Homepage />, { wrapper: BrowserRouter });
+  const title = screen.getByRole("heading", {
+    name: "Welcome to the Blue Essence Emporium!",
+  });
+  const description = screen.getByText(
+    "Rito Games has blessed us with a special deal! Buy as many champions as you want for free! They're really real and will be delivered to your doorsteps!!!"
+  );
   const button = screen.getByRole("button", { name: "Shop" });
+  const image = screen.getByAltText(
+    "cursed pre-rework splash art of Lollipoppy"
+  );
+  expect(title).toBeInTheDocument();
+  expect(description).toBeInTheDocument();
   expect(button).toBeInTheDocument();
+  expect(image).toBeInTheDocument();
 });
