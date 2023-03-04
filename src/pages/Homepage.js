e2258daa3f1129a9c07d0da28908c65ab3fef501
Brian Wong <brianw7218@gmail.com>
2023-03-03 22:10:38 -0800

    Spruce up the homepage contents and add an image of Lollipoppy

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <div>
-      <h1>Welcome!</h1>
+      <h1>Welcome to the Blue Essence Emporium!</h1>
+      <p>
+        Rito Games has blessed us with a special deal! Buy as many champions as
+        you want for free! They're really real and will be delivered to your
+        doorsteps!!!
+      </p>
+      <img
+        src="https://preview.redd.it/2hj44387pmp61.png?width=640&crop=smart&auto=webp&s=be1cff6868894962f63b549d17279a2c38afaf13"
+        alt="cursed pre-rework splash art of Lollipoppy"
+      />
       <Link to="/shop">
         <button>Shop</button>
       </Link>
+      <p>This is not real. All assets belong to Riot Games.</p>
     </div>
   );
 };
