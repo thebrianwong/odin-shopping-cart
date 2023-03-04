168315074b4bc6a7edb2dcafa5a090700a43e058
Brian Wong <brianw7218@gmail.com>
2023-03-03 21:51:55 -0800

    Add a fake loading screen when placing an order

--- src/pages/OrderConfirmation.js
+++ src/pages/OrderConfirmation.js
@@ -1,17 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const OrderConfirmation = ({ clearShoppingCart }) => {
+  const [inFakeLoading, setInFakeLoading] = useState(true);
+  const setTimer = (delay) => {
+    new Promise((resolve) => setTimeout(resolve, delay)).then((resolve) =>
+      setInFakeLoading(resolve, false)
+    );
+  };
   useEffect(() => {
     clearShoppingCart();
+  }, []);
+  useEffect(() => {
+    setTimer((Math.random() + 1) * 1000);
   }, []);
   return (
     <>
       <h1>Order Confirmation</h1>
-      <h3>Congratulations, your order has been placed!</h3>
-      <p>
-        You will soon receive shipping details for your order. Click{" "}
-        <a href="https://tinyurl.com/43phznsv">here</a> for instructions.
-      </p>
+      {inFakeLoading ? (
+        <img src="https://i.imgur.com/QRUPiHQ.gif" />
+      ) : (
+        <>
+          <h3>Congratulations, your order has been placed!</h3>
+          <p>
+            You will soon receive shipping details for your order. Click{" "}
+            <a href="https://tinyurl.com/43phznsv">here</a> for instructions.
+          </p>
+        </>
+      )}
     </>
   );
 };
