c04e26ea5827e56bf70116077f09139607fee2f4
Brian Wong <brianw7218@gmail.com>
2023-03-04 23:31:51 -0800

    Add class names and styling to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,21 +3,24 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <div className="homepage">
-      <h1>Welcome to the Blue Essence Emporium!</h1>
-      <p>
+      <h1 className="homepage-title">Welcome to the Blue Essence Emporium!</h1>
+      <p className="homepage-description-1">
         Rito Games has blessed us with a special deal! Buy as many champions as
         you want for free! They're really real and will be delivered to your
         doorsteps!!!
       </p>
-      <p>Don't take our word for it, ask this satisfied customer!!1!!!!11!!</p>
+      <p className="homepage-description-2">
+        Don't take our word for it, ask this satisfied customer!!1!!!!11!!
+      </p>
       <img
+        className="homepage-image"
         src={require("../assets/images/lollipoppy_og.png")}
         alt="Cursed pre-rework splash art of Lollipoppy."
       />
       <Link to="/shop">
-        <button>Shop</button>
+        <button className="homepage-shop-button">Shop</button>
       </Link>
-      <p>
+      <p className="homepage-credits">
         This is not real. All assets belong to Riot Games. Lollipoppy Kench is
         from this{" "}
         <a href="https://www.reddit.com/r/leagueoflegends/comments/dmvin3/lollipoppy_is_on_sale_right_now_alongside_all_the/">
