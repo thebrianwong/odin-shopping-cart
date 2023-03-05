8ee4772856c9eab3fd831503bf0d28b2f1b7d6c4
Brian Wong <brianw7218@gmail.com>
2023-03-04 16:54:32 -0800

    Add class names and adjust HTML structure for modal styling

--- src/components/ChampionModal.js
+++ src/components/ChampionModal.js
@@ -17,41 +17,58 @@ const ChampionModal = ({ gameVersion, championData, hideModal, addToCart }) => {
     fetchData();
   }, []);
   return !loadingData ? (
-    <div>
-      <img
-        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championData.id}_0.jpg`}
-        alt={`Loading screen banner of ${championData.id}.`}
-      />
-      <h1>{championData.name}</h1>
-      <p>{championData.title}</p>
-      <p>{championLore}</p>
-      <p>Price: FREE</p>
-      <button
-        onClick={() => {
-          if (quantity >= 11) {
-            setQuantity(quantity - 10);
-          }
-        }}
-      >
-        -10
-      </button>
-      <input
-        type="number"
-        min="1"
-        value={quantity}
-        onChange={(e) => setQuantity(e.target.value)}
-      />
-      <button onClick={() => setQuantity(quantity + 10)}>+10</button>
-      <button
-        onClick={() => {
-          if (quantity !== "" && quantity > 0) {
-            addToCart(championData.id, quantity);
-          }
-        }}
-      >
-        Add To Cart
-      </button>
-      <button onClick={hideModal}>X</button>
+    <div className="champion-modal-background">
+      <div className="champion-modal">
+        <div className="modal-left">
+          <img
+            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championData.id}_0.jpg`}
+            alt={`Loading screen banner of ${championData.id}.`}
+          />
+        </div>
+        <div className="modal-right">
+          <button className="modal-close-button" onClick={hideModal}>
+            X
+          </button>
+          <div className="modal-champion-info">
+            <div className="modal-non-lore">
+              <h1 className="modal-name">{championData.name}</h1>
+              <p className="modal-title">{championData.title}</p>
+            </div>
+            <p className="modal-lore">{championLore}</p>
+          </div>
+          <div className="modal-cart-section">
+            <p className="modal-price">Price: FREE</p>
+            <div className="modal-quantity-buttons">
+              <button
+                onClick={() => {
+                  if (quantity >= 11) {
+                    setQuantity(quantity - 10);
+                  }
+                }}
+              >
+                -10
+              </button>
+              <input
+                type="number"
+                min="1"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+              <button onClick={() => setQuantity(quantity + 10)}>+10</button>
+            </div>
+            <button
+              className="modal-add-to-cart-button"
+              onClick={() => {
+                if (quantity !== "" && quantity > 0) {
+                  addToCart(championData.id, quantity);
+                }
+              }}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   ) : null;
 };
